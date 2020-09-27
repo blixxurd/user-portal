b1bfde91a7d30146d789e822b3e9190fe6c322ca
Aaron Bartholomew <abartholomew@blixhost.com>
2020-09-27 16:50:55 -0700

    Final Action Fixes

--- README.md
+++ README.md
@@ -12,3 +12,24 @@ Server implementation, written in NodeJS & Express.
 
 ## [client](client)
 Client implementation, written in VueJS.
+
+## Remaining Tasks for ideal MVP
+
+**Server**
+- Server side verification of fields existing for login.
+- Recovery entries should check or existing entry so that users can't pollute the table
+- Throw 401 error for CORS
+- Verify that all client errors are
+- Re-send method for verifications
+
+**Client**
+- Form validation handler
+- Token saving
+- Authenticated POST in API Controller
+- Profile Editing
+- Resend Method for new account verification
+
+## Mid Term Tasks for full Open Source Release
+- Token Validation & Storage with HTTP-Only cookie, instead of LocalStorage
+- Refresh Token functionality
+- Return both an error code & a error message for API Errors 
--- client/src/pages/Actions.vue
+++ client/src/pages/Actions.vue
@@ -24,20 +24,21 @@
         <div class="password-reset" v-if="$route.params.action == 'reset-password'">
           <PasswordChange :token="actionId" />
         </div>
-        <div class="error-maggaging" v-else>
+        <div class="error-messaging" v-else>
             <Alerts
               class="mt-4 mb-3"
               type="danger"
-              :message="`This ${type} link has expired`"
+              :message="`This ${type} link is no longer active.`"
               :icon="true"
               title="Expired"
-              v-if="verificationData.expired || verificationData.handled"
+              v-if="verificationData.expired"
             />
-            <div class="back-to-account my-10">
+            <div class="back-to-account my-10" v-if="verificationData.expired">
               <router-link to="/" class="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm leading-5 font-medium rounded-md text-white bg-orange-500 hover:bg-orange-400 focus:outline-none focus:border-orange-700 focus:shadow-outline-orange active:bg-orange-700 transition duration-150 ease-in-out">
                 Back to Login
               </router-link>
             </div>
+            <Error v-if="verificationData.handled" />
         </div>
 
 
